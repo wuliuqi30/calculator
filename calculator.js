40f18499219041e75f2c681c41a08b36365a2f70
Hersh <sanganihersh@gmail.com>
2024-07-22 00:31:21 -0400

    Complete initial styling

--- calculator.js
+++ calculator.js
@@ -32,38 +32,39 @@ console.log(divide(2, 9));
 
 const operate = function (calculatorState) {
     calculatorState.calculationResult = calculatorState.operatorToBeUsed.function(calculatorState.firstNumber, calculatorState.secondNumber);
-    calculatorState.calculateButtonJustPressed = true;
+
     return calculatorState.calculationResult;
 };
 
 const operators = {
     add: {
-        function:add,
-        name:'add',
-        symbol:'+'
+        function: add,
+        name: 'add',
+        symbol: '+'
     },
     subtract: {
-        function:subtract,
-        name:'subtract',
-        symbol:'-'
+        function: subtract,
+        name: 'subtract',
+        symbol: '-'
     },
     multiply: {
-        function:multiply,
-        name:'multiply',
-        symbol:'x'
+        function: multiply,
+        name: 'multiply',
+        symbol: 'x'
     },
     divide: {
-        function:divide,
-        name:'divide',
-        symbol:'÷'
+        function: divide,
+        name: 'divide',
+        symbol: '÷'
     }
 }
 
 const lastButtonPressedEnum = {
-    clear:'clear',
-    number:'number',
-    calculate:'calculate',
-    operator:'operator'
+    clear: 'clear',
+    number: 'number',
+    calculate: 'calculate',
+    operator: 'operator',
+    decimalPoint: 'decimalPoint'
 }
 
 
@@ -87,8 +88,8 @@ let b = 5;
 const putInDisplayWindow = function (char) {
     // Input should be a string, typically a single character
     let displayWindow = document.querySelector(".display");
-    
-    if (calculatorState.lastButtonPressed === lastButtonPressedEnum.operator || 
+
+    if (calculatorState.lastButtonPressed === lastButtonPressedEnum.operator ||
         calculatorState.lastButtonPressed === lastButtonPressedEnum.calculate) {
         displayWindow.value = char; // Clear the display value with new data
     } else {
@@ -98,7 +99,6 @@ const putInDisplayWindow = function (char) {
 
 // Create Calculator In Javascript: 
 
-const body = document.querySelector('body');
 
 // Global Object Holding Important State Variables::
 const calculatorState = {
@@ -114,6 +114,36 @@ const calculatorState = {
 // lastButtonPressed can be: 
 
 
+// Create The Calculator: 
+const body = document.querySelector('body');
+
+
+const calculatorContainer = document.createElement("div");
+calculatorContainer.className = "calculatorContainer";
+body.appendChild(calculatorContainer)
+
+// Create the display:
+const divDisplayContainer = document.createElement('input');
+divDisplayContainer.className = "display";
+calculatorContainer.appendChild(divDisplayContainer);
+
+// Create Clear Button
+
+const clrbtn = document.createElement("button");
+clrbtn.setAttribute("class", "button");
+clrbtn.setAttribute("id","clearButton")
+clrbtn.textContent = "Clear";
+clrbtn.addEventListener("click", () => {
+    let displayWindow = document.querySelector(".display");
+    displayWindow.value = '';
+    calculatorState.secondNumber = null;
+    calculatorState.firstNumber = null;
+    calculatorState.lastButtonPressed = lastButtonPressedEnum.clear;
+}
+)
+calculatorContainer.appendChild(clrbtn)
+// Create the Divide Button
+
 // Create 10 buttons
 
 for (i = 0; i <= 9; i++) {
@@ -127,8 +157,23 @@ for (i = 0; i <= 9; i++) {
         putInDisplayWindow(btn.textContent)
         calculatorState.lastButtonPressed = lastButtonPressedEnum.number;
     });
-    body.appendChild(btn);
+    calculatorContainer.appendChild(btn);
 }
+
+// Create a button for the . (decimal point)
+
+const btnDot = document.createElement("button");
+btnDot.setAttribute("class", "button")
+btnDot.textContent = '.';
+btnDot.addEventListener("click", () => {
+    let displayWindow = document.querySelector(".display");
+    if (!displayWindow.value.includes('.') || 
+    calculatorState.lastButtonPressed === lastButtonPressedEnum.calculate) {
+        putInDisplayWindow(btnDot.textContent)
+        calculatorState.lastButtonPressed = lastButtonPressedEnum.decimalPoint;
+    }
+});
+calculatorContainer.appendChild(btnDot);
 
 // Create Buttons for the operators
 
@@ -138,10 +183,10 @@ for (const op in operators) {
     let subObj = operators[op];
     btn.textContent = subObj.symbol;
     btn.value = subObj.name;
-    body.appendChild(btn);
+    calculatorContainer.appendChild(btn);
 
     btn.addEventListener("click", () => {
-        
+
         let displayWindow = document.querySelector(".display");
         calculatorState.firstNumber = Number(displayWindow.value);
         calculatorState.operatorToBeUsed = subObj;
@@ -150,42 +195,40 @@ for (const op in operators) {
     })
 }
 
-// Create Clear Button
 
-const clrbtn = document.createElement("button");
-clrbtn.setAttribute("class", "button");
-clrbtn.textContent = "Clear";
-clrbtn.addEventListener("click", () => {
-    let displayWindow = document.querySelector(".display");
-    displayWindow.value = '';
-    calculatorState.lastButtonPressed = lastButtonPressedEnum.clear;
-}
-)
 
 // Create Compute Button (=)
 const computeBtn = document.createElement("button");
 computeBtn.setAttribute("class", "button");
 computeBtn.textContent = "=";
 computeBtn.addEventListener("click", () => {
-    // Only do the calculation if the last thing you did was click a number
-    if (calculatorState.lastButtonPressed === lastButtonPressedEnum.number)
-        {
-    let displayWindow = document.querySelector(".display");
-    calculatorState.secondNumber = Number(displayWindow.value);
-    calculatorState.calculationResult = operate(calculatorState);
-    calculatorState.lastButtonPressed = lastButtonPressedEnum.calculate;
-    // Put the full calculation in the box to the left of the result
-
-    putInDisplayWindow(calculatorState.calculationResult);
+    // Only do the calculation if the last thing you did was click a number. 
+    // And if there is a "first number" that was previously calculated during an "operator" click
     
-    } 
+    if (calculatorState.lastButtonPressed === lastButtonPressedEnum.number && 
+        calculatorState.firstNumber
+    ) {
+        let displayWindow = document.querySelector(".display");
+        calculatorState.secondNumber = Number(displayWindow.value);
+        calculatorState.calculationResult = operate(calculatorState);
+        calculatorState.lastButtonPressed = lastButtonPressedEnum.calculate;
+        // Put the full calculation in the box to the left of the result
+
+        putInDisplayWindow(calculatorState.calculationResult);
+
+        // Reset the "first" and "second" number so that the calculator thinks it still needs 
+        // to click an operator before a new calculation can be performed
+        calculatorState.firstNumber = null;
+        calculatorState.secondNumber = null; 
+
+    }
 
 }
 )
-body.appendChild(computeBtn)
+calculatorContainer.appendChild(computeBtn)
 
 
-body.appendChild(clrbtn)
+
 
 
 // Things to Add: 
